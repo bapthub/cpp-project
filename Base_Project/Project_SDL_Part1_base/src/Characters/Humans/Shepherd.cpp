c52aca3a1b6eefbf8df324376f7c7f3962c8b1f9
dsarmiento <dsarmiento@DSA-PF26GYD1.corp.wallix.com>
2023-01-17 15:02:23 +0100

    avoid the shepherd to go out of frame

--- Base_Project/Project_SDL_Part1_base/src/Characters/Humans/Shepherd.cpp
+++ Base_Project/Project_SDL_Part1_base/src/Characters/Humans/Shepherd.cpp
@@ -14,9 +14,16 @@ Shepherd::Shepherd(SDL_Surface *window_surface_ptr) : human(
 
 }
 
-// Update the animal's position based on the given offsets
+// Update the Shepherd's position based on the given offsets
 void Shepherd::move(int x_offset, int y_offset)
 {
-    point.x += x_offset;
-    point.y += y_offset;
+    if (point.x + x_offset > 0 && point.x + SHEPHERD_WIDTH + x_offset < frame_width)
+    {
+        point.x += x_offset;
+    }
+    
+    if (point.y + y_offset > 0 && point.y + SHEPHERD_WIDTH + y_offset < frame_height)
+    {
+        point.y += y_offset;
+    }
 }
