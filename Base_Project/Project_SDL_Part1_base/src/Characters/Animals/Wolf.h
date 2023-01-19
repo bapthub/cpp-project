3005ad8760b4e20b8753e4637d08d6e7607380ee
Arthur Plebs <arthur.45@live.fr>
2023-01-19 13:36:18 +0100

    fix wolf's bugs

--- Base_Project/Project_SDL_Part1_base/src/Characters/Animals/Wolf.h
+++ Base_Project/Project_SDL_Part1_base/src/Characters/Animals/Wolf.h
@@ -16,8 +16,8 @@ const int spawn_wolf_life = 5;
 class Wolf : public Animal {
 private: 
     int distance_nearest_sheep = -1;
-    static const int H_AREA_EFFECT = wolf_height * 3;
-    static const int W_AREA_EFFECT = wolf_width * 3;
+    static const int H_AREA_EFFECT = wolf_height * 4;
+    static const int W_AREA_EFFECT = wolf_width * 4;
 public:
     explicit Wolf(SDL_Surface *window_surface_ptr);
 
@@ -29,7 +29,6 @@ public:
 
     void collide(Animal& animal, std::vector<std::shared_ptr<Animal>>& animals) override;
     
-    int change_direction(int cor_to_change,int cor_to_check);
     int avoid_dog(Animal& animal);
     bool hunt(Animal& animal);
 };
